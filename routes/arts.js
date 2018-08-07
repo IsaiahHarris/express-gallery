1564301b1ce8b4e4eff6cbf4a7cf17b5c13f2955
Isaiah Harris <harrisfambam@gmail.com>
2018-08-06 19:38:13 -1000

    newpage

--- routes/arts.js
+++ routes/arts.js
@@ -30,11 +30,15 @@ router.route('/')
       })
   })
 
+router.get('/new', (req, res) => {
+  res.send('newpage')
+})
+
 router.route('/:id')
   .get((req, res) => {
     let id = req.params.id;
     return Art
-      .query({ where: { id } })
+      .query({ where: { id: id } })
       .fetchAll()
       .then(arts => {
         if (!arts) {
@@ -44,7 +48,7 @@ router.route('/:id')
         }
       })
       .catch(err => {
-        res.json({"message": err.message})
+        res.json({ "message": err.message })
       })
   })
   .put((req, res) => {
@@ -53,9 +57,9 @@ router.route('/:id')
     return new Art({ id: id })
       .save({ author, link, description })
       .then(arts => {
-        if(!arts){
-          res.status(404).json({"message": "artwork does not exist"})
-        }else {
+        if (!arts) {
+          res.status(404).json({ "message": "artwork does not exist" })
+        } else {
           res.json(arts)
         }
       })
@@ -63,6 +67,19 @@ router.route('/:id')
         return res.json({ "message": err.message })
       })
   })
+  .delete((req, res) => {
+    let id = req.params.id;
+    return new Art({ id: id })
+      .destroy()
+      .then(result => {
+        res.send('has been deleted')
+      })
+      .catch(err => {
+        res.send('there has been an error')
+      })
+  })
+
+
 
 
 module.exports = router;
