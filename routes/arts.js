e82c0e864f79afac1770739a1ee54531f88be657
Isaiah Harris <harrisfambam@gmail.com>
2018-08-06 18:47:40 -1000

    get working

--- routes/arts.js
+++ routes/arts.js
@@ -3,33 +3,31 @@ const router = express.Router();
 const Art = require('../models/Art');
 
 router.route('/')
-.get((req,res)=>{
-  return Art
-  .fetchAll()
-  .then(result=>{
-    if(!result || !result.rowCount){
-      res.send('there are no posts')
-    }
-    return result;
+  .get((req, res) => {
+    return Art
+      .fetchAll()
+      .then(arts => {
+        if (!arts) {
+          return res.json({ "message": "there is no art" })
+        } else {
+          return res.json(arts)
+        }
+      })
+      .catch(err => {
+        return res.json({ "message": err.message })
+      })
   })
-  .then(arts=>{
-    return res.json(arts)
+  .post((req, res) => {
+    let { author, link, description } = req.body;
+    link = link.toLowerCase();
+    return new Art({ author, link, description })
+      .save()
+      .then(post => {
+        return res.json(post)
+      })
+      .catch(err => {
+        return res.json({ "message": err.message })
+      })
   })
-  .catch(err=>{
-    return res.json({"message": err.message})
-  })
-})
-.post((req,res)=>{
-  let { author, link, description } = req.body;
-  link = link.toLowerCase();
-  return new Art({ author, link, description})
-  .save()
-  .then(post=>{
-    return res.json(post)
-  })
-  .catch(err=>{
-    return res.json({"message": err.message})
-  })
-})
 
 module.exports = router;
