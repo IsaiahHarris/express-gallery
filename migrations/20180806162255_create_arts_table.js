a8d4e8cc3f4ef51822d4077b701592bdc44243a9
Isaiah Harris <harrisfambam@gmail.com>
2018-08-06 16:31:50 -1000

    created arts table

--- migrations/20180806162255_create_arts_table.js
+++ migrations/20180806162255_create_arts_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('arts', table=>{
+    table.increments();
+    table.string('link')
+    table.text('description');
+    table.integer('author_id').references('users.id');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('arts');
 };
