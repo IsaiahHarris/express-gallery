1b2ce06cdaf93107ecf9306d45b63ab921111d65
Isaiah Harris <harrisfambam@gmail.com>
2018-08-07 18:22:39 -1000

    login with username not password

--- migrations/20180806160245_create_users_table.js
+++ migrations/20180806160245_create_users_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table=>{
     table.increments();
-    table.string('name', 50).notNullable();
+    table.string('username', 50).notNullable();
     table.string('email').unique().notNullable();
     table.string('password').notNullable();
     table.timestamps(true, true);
